998d849d6a5371d47eec0c6faa82aeb51c01415b
SamuelRnn <ricardo.reinn@outlook.com>
2023-02-22 18:28:38 -0500

    feat(/views/register): UI template finished

--- client/src/views/register.jsx
+++ client/src/views/register.jsx
@@ -1,13 +1,26 @@
 import styles from "../styles/register.module.css";
-import { BiChevronsLeft } from "react-icons/bi";
+import { BiChevronLeft } from "react-icons/bi";
+import { BsFillFileEarmarkTextFill } from "react-icons/bs";
 import { useState } from "react";
 import image_placeholder from "../assets/no_image.jpg";
+import { registerFormModel } from "../models";
+// import { getImageData } from "../utils";
 
 export default function Register() {
+  // form handlers
   const [form, setForm] = useState({
     entity_type: "",
+    full_name: "",
+    identification_number: null,
+    country: "",
+    entity_type: "",
+    entity_name: "",
+    phone: null,
+    email: "",
+    password: "",
+    confirmed_password: "",
+    image: null,
   });
-  const [imagePreview, setImagePreview] = useState(image_placeholder);
   const onChange = (name, value) => {
     setForm((data) => {
       return { ...data, [name]: value };
@@ -15,60 +28,90 @@ export default function Register() {
   };
   const onSubmit = (event) => {
     event.preventDefault();
-    event.target.reset();
+    console.table(form);
+    // event.target.reset();
+  };
+
+  // image preview handlers
+  const [imagePreview, setImagePreview] = useState(null);
+  const onImageChange = ({ target }) => {
+    const url = URL.createObjectURL(target.files[0]);
+    setImagePreview(url);
   };
 
   return (
     <>
       <header className="bg-zinc-300  py-8">
         <div className="mx-auto w-form relative">
-          <button className="flex items-center gap-x-2 absolute top-0 left-0 rounded-full max-sm:bg-zinc-100 hover:bg-zinc-100 ease-out transition-colors pl-1 py-1 pr-2">
+          {/* login anchor */}
+          <button className="flex items-center gap-x-2 absolute top-0 left-0 rounded-full max-sm:bg-zinc-100/60 hover:bg-zinc-100 ease-out transition-colors pl-1 py-1 pr-2">
             <div className="w-6 h-6 sm:w-8 sm:h-8 bg-zinc-400 text-2xl grid place-items-center rounded-full">
-              <BiChevronsLeft />
+              <BiChevronLeft />
             </div>
-            <span className="text-sm">Volver</span>
+            <span className="text-sm">Login</span>
           </button>
 
-          <div className="pt-8 flex flex-col justify-center items-center text-sm">
-            <input type="file" accept="image/*" hidden id="image" />
-            <img
-              src={imagePreview}
-              alt="profile picture"
-              className="rounded-full h-28 w-28 bg-zinc-800 mb-2 shadow-lg border-2"
-            />
-            <label htmlFor="image" className="hover:underline text-zinc-600">
-              Subir foto
+          {/* image load */}
+          <div className="pt-8">
+            <label
+              htmlFor="image"
+              className="hover:underline lg:hover:bg-zinc-200/80 flex flex-col items-center justify-center w-fit mx-auto p-2 rounded-lg text-sm text-zinc-600 transition-colors ease-out duration-300"
+            >
+              <input
+                type="file"
+                accept="image/*"
+                hidden
+                id="image"
+                onChange={onImageChange}
+              />
+              <img
+                src={imagePreview || image_placeholder}
+                alt="profile picture"
+                className="rounded-full h-28 w-28 bg-zinc-800 mb-2 shadow-lg"
+              />
+              <span>Subir foto</span>
             </label>
           </div>
         </div>
       </header>
 
-      <section className="">
+      <section>
         <form
           className="flex flex-col gap-y-4 mt-4 mb-8  mx-auto w-form"
           onSubmit={onSubmit}
         >
+          <h2 className="mb-4 text-zinc-700 flex gap-x-2 items-center">
+            <BsFillFileEarmarkTextFill className="text-sm" />
+            <span>Completa el formulario con tus datos</span>
+          </h2>
+
           <div>
             <label className={styles.label}>Nombres y apellidos</label>
             <input
               type="text"
-              onChange={({ target }) => onChange("full_name", target.value)}
+              onChange={({ target }) =>
+                onChange(registerFormModel.full_name, target.value)
+              }
             />
           </div>
+
           <div>
             <label className={styles.label}>Número de identificación</label>
             <input
               type="text"
               onChange={({ target }) =>
-                onChange("identification_number", target.value)
+                onChange(registerFormModel.identification_number, target.value)
               }
             />
           </div>
+
           <div>
             <label className={styles.label}>País de origen</label>
             <input
               type="text"
-              onChange={({ target }) => onChange("country", target.value)}
+              onChange={({ target }) =>
+                onChange(registerFormModel.country, target.value)
+              }
             />
           </div>
 
@@ -76,7 +119,7 @@ export default function Register() {
             <label className={styles.label}>
               Seleccione su tipo de entidad
             </label>
-            <div className="p-2 bg-zinc-300 rounded-md mt-1 h-12">
+            <div className="p-2 bg-zinc-300 rounded-md mt-1 h-12 text-sm">
               <div className="rounded-[4px] h-full relative z-10">
                 <div
                   className={`absolute w-1/2 h-full bg-zinc-100 transition-transform duration-200 ${
@@ -85,14 +128,18 @@ export default function Register() {
                 ></div>
                 <div className="w-full grid grid-cols-2 rounded-[4px] h-full z-20 absolute">
                   <span
-                    onClick={() => onChange("entity_type", "public")}
+                    onClick={() =>
+                      onChange(registerFormModel.entity_type, "public")
+                    }
                     className="text-zinc-700 grid place-items-center cursor-default md:cursor-pointer"
                     disabled={form.entity_type === "public"}
                   >
                     Pública
                   </span>
                   <span
-                    onClick={() => onChange("entity_type", "private")}
+                    onClick={() =>
+                      onChange(registerFormModel.entity_type, "private")
+                    }
                     className="text-zinc-700 grid place-items-center cursor-default md:cursor-pointer"
                     disabled={form.entity_type === "private"}
                   >
@@ -104,20 +151,57 @@ export default function Register() {
           </div>
           <div>
             <label className={styles.label}>Nombre de su entidad</label>
-            <input type="text" onChange={onChange} />
+            <input
+              type="text"
+              onChange={({ target }) =>
+                onChange(registerFormModel.entity_name, target.value)
+              }
+            />
           </div>
+
           <div>
             <label className={styles.label}>Celular</label>
-            <input type="text" onChange={onChange} />
+            <input
+              type="text"
+              onChange={({ target }) =>
+                onChange(registerFormModel.phone, target.value)
+              }
+            />
           </div>
+
           <div>
             <label className={styles.label}>E-mail</label>
-            <input type="text" onChange={onChange} />
+            <input
+              type="text"
+              onChange={({ target }) =>
+                onChange(registerFormModel.email, target.value)
+              }
+            />
+          </div>
+
+          <div>
+            <label className={styles.label}>Contraseña</label>
+            <input
+              type="text"
+              onChange={({ target }) =>
+                onChange(registerFormModel.password, target.value)
+              }
+            />
+          </div>
+
+          <div>
+            <label className={styles.label}>Confirmar contraseña</label>
+            <input
+              type="text"
+              onChange={({ target }) =>
+                onChange(registerFormModel.confirmed_password, target.value)
+              }
+            />
           </div>
 
           <input
             type="submit"
-            value="Registrate"
+            value="Registrarse"
             className="bg-zinc-800 text-zinc-200 rounded-full h-14 mt-4"
           />
         </form>
