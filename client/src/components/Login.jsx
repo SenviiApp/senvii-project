07ef69703fc4bcc80fdf8b118d6758d686b03135
AgDelfino <agustin.delfino92@gmail.com>
2023-02-22 13:59:11 -0300

    feat(Login.jsx) template created

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>
-        <h1>SOY EL LOGIN</h1>
-    </div>
-  )
-}
+    <div className="h-screen bg-slate-800 grid items-center">
+      <div className="bg-zinc-500 text-white w-[90%] mx-auto text-center p-10 space-y-2 flex flex-col justify-center items-center rounded-lg">
+        <h1 className="text-3xl">SENVI</h1>
+        <h2 className="">Tu consultor vial inteligente</h2>
 
-export default Login
+        <form action="" className="flex flex-col space-y-2">
+          <input type="text" placeholder="Ingrese su email" className="p-2 border-2 border-blue-400"/>
+          <input type="text" placeholder="Ingrese su password" className="p-2 border-2 border-blue-400"/>
+          <button className="p-2 bg-blue-400 rounded-full shadow-lg">Inicia Sesión</button>
+        </form>
+      <div className="flex text-xs space-x-1">
+        <p>¿Olvidaste tu contraseña?</p>
+        <a href="" className="text-blue-300">Click aquí</a>
+      </div>
+      <div className="flex text-xs space-x-1">  
+        <p>¿No tenés cuenta?</p>
+        <a href="" className="text-blue-300">Regístrate aquí</a>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
