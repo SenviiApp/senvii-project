676ebdd265194d4ba4ab766d76eb1f3e0d1115cd
AgDelfino <agustin.delfino92@gmail.com>
2023-02-22 17:08:30 -0300

    mod(views/login.jsx): added links

--- client/src/views/login.jsx
+++ client/src/views/login.jsx
@@ -1,30 +1,44 @@
 import React from "react";
+import PopUp from "../components/PopUp"
 
 const Login = () => {
-
-
   return (
     <div className="h-screen bg-slate-800 grid items-center shadow-lg loginBg">
       <div className="backdrop-blur-sm bg-slate-800/60 text-white w-[90%] mx-auto text-center p-10 space-y-2 flex flex-col justify-center items-center rounded-lg">
-        <h1 className="text-3xl">SENVI</h1>
-        <h2 className="">Tu consultor vial inteligente</h2>
+        <div>
+          <h1 className="text-3xl">SENVI</h1>
+          <h2 className="">Tu consultor vial inteligente</h2>
+        </div>
 
-        <form action="" className="flex flex-col space-y-2">
-          <input type="text" placeholder="Ingrese su email" className="p-2 border-2 border-blue-400"/>
-          <input type="text" placeholder="Ingrese su password" className="p-2 border-2 border-blue-400"/>
-          <button className="p-2 bg-blue-400 rounded-full shadow-lg">Inicia Sesión</button>
+        <form action="" className="flex flex-col gap-y-1">
+          <input
+            type="text"
+            placeholder="Ingrese su email"
+            className="p-2 border-2 border-blue-400 text-slate-900 font-medium"
+          />
+          <input
+            type="text"
+            placeholder="Ingrese su password"
+            className="p-2 border-2 border-blue-400 text-slate-900 font-medium"
+          />
+          <input
+            type="submit"
+            value="Iniciar Sesión"
+            className="p-2 bg-blue-400 rounded-full shadow-lg mt-4"
+          />
         </form>
-      <div className="flex text-xs space-x-1">
-        <p>¿Olvidaste tu contraseña?</p>
-        <a href="" className="text-blue-300">Click aquí</a>
+        <div className="text-sm flex flex-col pt-4">
+          <a href="/forgot-password" className="text-blue-200">
+            ¿Olvidaste tu contraseña?
+          </a>
+          <a href="/register" className="text-blue-200 ">
+            ¿No tenés cuenta?
+          </a>
+        </div>
       </div>
-      <div className="flex text-xs space-x-1">  
-        <p>¿No tenés cuenta?</p>
-        <a href="" className="text-blue-300">Regístrate aquí</a>
-      </div>
-      </div>
-    </div>
 
+      <PopUp/>
+    </div>
   );
 };
 
