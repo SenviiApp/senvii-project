da2ebe944b174a3516f7f4347e8cfd66afb4e921
AgDelfino <agustin.delfino92@gmail.com>
2023-02-22 17:06:16 -0300

    feat(ResetPassword.jsx) template fin

---
+++ client/src/views/ResetPassword.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const ResetPassword = () => {
+  return (
+    <div className="h-screen bg-slate-800 grid items-center shadow-lg loginBg">
+      <div className="backdrop-blur-sm bg-slate-800/60 text-white w-[90%] mx-auto text-center p-10 space-y-2 flex flex-col justify-center items-center rounded-lg">
+        <div>
+          <h1 className="text-xl">Cambiar contraseña</h1>
+
+        </div>
+
+        <form action="" className="flex flex-col gap-y-1">
+          <input
+            type="text"
+            placeholder="Nueva contraseña"
+            className="p-2 border-2 border-blue-400 text-slate-900 font-medium"
+          />
+          <input
+            type="text"
+            placeholder="Reingrese contraseña"
+            className="p-2 border-2 border-blue-400 text-slate-900 font-medium"
+          />
+          <input
+            type="submit"
+            value="Confirmar"
+            className="p-2 bg-blue-400 rounded-full shadow-lg mt-4"
+          />
+        </form>
+        <div className="text-sm flex flex-col pt-4">
+          <a href="/login" className="text-blue-200 ">
+            Login
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+    
+export default ResetPassword
