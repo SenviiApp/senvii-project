bcd34b177eaee8550cad7afb7aa8552e399b6f13
SamuelRnn <ricardo.reinn@outlook.com>
2023-02-23 14:24:02 -0500

    loader added on register

---
+++ client/src/components/MiniLoader.jsx
@@ -0,0 +1,8 @@
+export default function MiniLoader() {
+  return (
+    <div
+      className="inline-block h-5 w-5 animate-spin rounded-full border-[3px] border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      role="status"
+    />
+  );
+}
