36bb4c3392d3fa4927d96219afd7765fcb9999f4
SamuelRnn <ricardo.reinn@outlook.com>
2023-02-23 17:51:51 -0500

    register form parsing

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,6 +1,6 @@
+export { default as ForgotPassword } from "./ForgotPassword";
 export { default as Home } from "./Home";
 export { default as Login } from "./Login";
+export { default as MailConfirmed } from "./MailConfirmed";
 export { default as Register } from "./Register";
-export {default as ForgotPassword} from './ForgotPassword'
-export {default as ResetPassword} from "./ResetPassword"
-export {default as MailConfirmed} from "./MailConfirmed"
+export { default as ResetPassword } from "./ResetPassword";
