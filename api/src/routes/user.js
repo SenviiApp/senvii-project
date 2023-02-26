1a7d81f13af9617a6f056b95d8f6ae306d722b11
Sojo506 <fabiansojowork@gmail.com>
2023-02-26 16:28:42 -0600

    add(api/src/(controllers/routes)): setting up update user

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -2,9 +2,10 @@ const { Router } = require("express");
 
 const router = new Router();
 
-const { getUser, changePassword } = require("../controllers/user");
+const { getUser, changePassword, editProfile } = require("../controllers/user");
 
 router.get("/:id", getUser);
 router.post("/change-password", changePassword);
+router.post("/edit-profile/:id", editProfile);
 
 module.exports = router;
