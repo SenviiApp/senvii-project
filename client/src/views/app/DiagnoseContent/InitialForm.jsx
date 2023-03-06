2282da672cbf8f7e7504049df91b826c59f1f623
Mailen93 <m.labarrere2@gmail.com>
2023-03-06 01:12:52 -0300

    mod(DiagnoseContent.InitialForm): controlled && added senvii animation

--- client/src/views/app/DiagnoseContent/InitialForm.jsx
+++ client/src/views/app/DiagnoseContent/InitialForm.jsx
@@ -4,17 +4,41 @@ import { SiTestinglibrary } from "react-icons/si";
 import { useState } from "react";
 
 const InitialForm = ({ setCurrent, setForm }) => {
+
   const interfaceForm = {
-    viaType: "",
-    entityName: "",
+    viaType: "Pública",
     viaUbication: "",
+    viaMeasure: "KM",
     viaLength: 0,
+    viaAge: 0,
+    viaPavment: "",
   };
+
   const [initForm, setInitForm] = useState(interfaceForm);
 
+  const handleUbicationChange = (e) => {
+    let ubication = e.target.value;
+    setInitForm({ ...initForm, viaUbication: ubication });
+  };
+
+  const handleViaLengthChange = (e) => {
+    let length = e.target.value;
+    setInitForm({ ...initForm, viaLength: length });
+  };
+
+  const handleViaAge = (e) => {
+    let age = e.target.value;
+    setInitForm({ ...initForm, viaAge: age });
+  };
+
+  const handlePavmentChange = (e) => {
+      let pavment = document.querySelector("input[type=radio][name=pavmentType]:checked")
+      let pavmentType = pavment.value
+      setInitForm({...initForm, viaPavment: pavmentType})
+  }   
+
   return (
-    <div className="h-screen w-full bg-white mx-auto flex flex-col gap-2">
-      <h1>Ayúdanos a completar la información de la vía</h1>{/* EL CABEZON TIENE QUE DECIR ESTO */}
+    <div className=" w-[90%] bg-white mx-auto flex flex-col gap-2 p-4 justify-center">
       <div className="flex flex-col gap-1">
         <h2>Tipo de vía:</h2>
         <div className="p-2 bg-dark-400 rounded-md mt-1 h-12 text-sm">
@@ -45,25 +69,100 @@ const InitialForm = ({ setCurrent, setForm }) => {
           </div>
         </div>
       </div>
-      
-      {/* NOMBRE DE LA ENTIDAD */}
+
+      {/* UBICACIÓN DE LA VÍA */}
       <div>
-        <h2>Nombre de la entidad:</h2>
-        <input value="EDITABLE"/>
+        <h2>Ubicación de la vía:</h2>
+        <input
+          value={initForm.viaUbication}
+          className="w-full bg-slate-400 p-2 outline-none"
+          name="viaUbication"
+          type="text"
+          onChange={handleUbicationChange}
+        />
+      </div>
+      {/* LONGITUD DE LA VÍA */}
+      <div>
+        <h2>Longitud de la vía:</h2>
+        <input
+          className="w-full bg-slate-400 p-2 outline-none"
+          type="number"
+          value={initForm.viaLength}
+          onChange={handleViaLengthChange}
+        />
+        <div className="p-2 bg-dark-400 rounded-md mt-1 h-12 text-sm">
+          <div className="rounded-[4px] h-full relative z-10">
+            <div
+              className={`absolute w-1/2 h-full bg-light-500 transition-transform duration-300 ease-out ${
+                initForm.viaMeasure === "ML" && "translate-x-[100%]"
+              } rounded-md`}
+            ></div>
+            <div className="w-full grid grid-cols-2 rounded-[4px] h-full z-20 absolute">
+              <span
+                onClick={() => setInitForm({ ...initForm, viaMeasure: "KM" })}
+                className="text-snow grid place-items-center cursor-default md:cursor-pointer"
+                disabled={initForm.viaType === "KM"}
+              >
+                KM
+              </span>
+              <span
+                onClick={() => setInitForm({ ...initForm, viaMeasure: "ML" })}
+                className="text-snow grid place-items-center cursor-default md:cursor-pointer"
+                disabled={initForm.viaType === "ML"}
+              >
+                ML
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* ANTIGÛEDAD DE LA VÍA */}
+      <div>
+        <h2>Antigüedad de la vía:</h2>
+        <input
+          value={initForm.viaAge}
+          type="text"
+          className="w-full bg-slate-400 p-2 outline-none"
+          onChange={handleViaAge}
+        />
+      </div>
+      {/* TIPO DE PAVIMENTO */}
+      <div className="">
+        <h2>Tipo de pavimento:</h2>
+        <p className="flex text-sm">
+          <label>Concreto</label>
+          <input
+            name="pavmentType"
+            type="radio"
+            value="Asfalto"
+            className="w-full bg-slate-400 p-2 outline-none"
+            onClick={handlePavmentChange}
+          />
+          <label htmlFor="">Asfalto</label>
+          <input
+            name="pavmentType"
+            type="radio"
+            value="Concreto"
+            className="w-full bg-slate-400 p-2 outline-none"
+            onClick={handlePavmentChange}
+          />
+          <label htmlFor="">Afirmado</label>
+          <input
+            name="pavmentType"
+            type="radio"
+            value="Afirmado"
+            className="w-full bg-slate-400 p-2 outline-none"
+            onClick={handlePavmentChange}
+          />
+        </p>
       </div>
       <motion.button
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 0.6, delay: 1 }}
-        onClick={() => {
-          setForm((form) => {
-            const newForm = { ...form };
-            console.log(newForm);
-            return newForm;
-          });
-          setCurrent("start");
-        }}
+        onClick={() => console.log(initForm)}
         className="bg-black text-white py-2 px-14 rounded-full"
+        disabled={initForm === interfaceForm}
       >
         Continuar
       </motion.button>
