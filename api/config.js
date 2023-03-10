17426f3e5359c5249a97440797155ce3da84a0f6
Sojo506 <fabiansojowork@gmail.com>
2023-03-10 15:27:48 -0600

    mod(api): back to postgresql

--- api/config.js
+++ api/config.js
@@ -1,15 +1,15 @@
+const PORT = process.env.PORT || 3001;
 const DB_HOST = process.env.DB_HOST || "localhost";
-const DB_NAME = process.env.DB_NAME || "senvii";
-const DB_USER = process.env.DB_USER || "root";
+const DB_USER = process.env.DB_USER || "postgres";
 const DB_PASSWORD = process.env.DB_PASSWORD || "mama";
-const PORT = 3001
-const NODE_ENV = process.env.NODE_ENV;
+const DB_NAME = process.env.DB_NAME || "senvii";
+const DB_PORT = process.env.DB_PORT || 5432;
 
 module.exports = {
   DB_HOST,
   DB_USER,
   DB_PASSWORD,
   DB_NAME,
-  NODE_ENV,
-  PORT
+  PORT,
+  DB_PORT,
 };
